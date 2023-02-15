0a4c343900926e6d0a073dc84d752b9f45a83173
irynakrykun <ira_franyk@ukr.net>
2023-02-15 16:47:37 +0200

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,21 +1,21 @@
 import Contact from 'components/Contact/Contact';
 import { useSelector } from "react-redux";
-import { getContacts } from 'redux/selectors';
+import { getContacts,getFilter } from 'redux/selectors';
 import { List, ListItem } from './ContactList.styled';
 
-// const normalizedFilter = filter.toLowerCase();
 
-//   // const visibleContacts = contacts.filter(contact =>
-//   //   contact.name.toLowerCase().includes(normalizedFilter)
+const filterContacts = (items, filter) =>
+  items.filter(item => item.name.toLowerCase().includes(filter.toLowerCase()));
 
 
 const ContactList = () => {
   const items = useSelector(getContacts)
-  
+  const filter = useSelector(getFilter)
+  const visibleContacts = filterContacts(items,filter)
 
   return (
     <List>
-      {items.map((item, id) => (
+      {visibleContacts.map((item, id) => (
         <ListItem key={id}>
           <Contact contact={item}  />
         </ListItem>
